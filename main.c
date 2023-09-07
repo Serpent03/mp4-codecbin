b3a9b7560c6e1c4489b2ac3bd0eaf5eaf928ac60
Serpent03 <dumbpilot03@gmail.com>
2023-09-07 23:12:46 +0530

    feat: logic

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ void print4ByteArray(byte_4* arr, int size)
 {
     for (int i = 0; i < size; i++)
     {
-        printf("->%04x\n", arr[i] / 1024);
+        printf("->%04x\n", arr[i]);
     }
 }
 
@@ -20,7 +20,7 @@ void printByteArray(byte* byteArr, int size)
 {
     for (int i = 0; i < size; i++)
     {
-        printf("->%02x\n", byteArr[i]);
+        printf("->%04x\n", byteArr[i]);
     }
 }
 
@@ -29,7 +29,7 @@ int main()
     int byteArrSize = 8;    
     byte pngsig[8] = {137, 80, 78, 71, 13, 10, 26, 10};
     byte* fileData = (byte *)malloc(byteArrSize * sizeof(byte));
-    byte_4 wh[1];
+    byte* wh = (byte *)malloc(13 * sizeof(byte));
 
     FILE* filePointer;
     filePointer = fopen("img.png", "rb");
@@ -37,15 +37,18 @@ int main()
     
     fread(fileData, 1, byteArrSize, filePointer);
     assert(memcmp(pngsig, fileData, sizeof(byte)) == 0);
-    fseek(filePointer, 15, SEEK_CUR);
-    fread(wh, 4, 1, filePointer);
-    print4ByteArray(wh, 1);
+    fseek(filePointer, 16, SEEK_SET);
+    fread(wh, 1, 13, filePointer);
+    printByteArray(wh, 13);
 
     printf("this is a png file");
 
     // REFER => https://gamedev.stackexchange.com/questions/102490/fastest-way-to-render-image-data-from-buffer
     // REFER => https://www.gamedev.net/forums/topic/683956-blit-a-byte-array-of-pixels-to-screen-in-sdl-fast/5320207/
+    // REFER => https://progbook.org/png.html
     // retro theme, menlo
+
+    // read each byte and gather little-endian 4 bytes from 4 different 1 bytes
 
     // get a RGBA array from this PNG file,
     // and then display it in the SDL blitting window
