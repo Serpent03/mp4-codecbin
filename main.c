62287358a2531fe74f13d9617831f935ccf42ee5
Serpent03 <dumbpilot03@gmail.com>
2023-09-08 22:31:16 +0530

    feat: struct for PNG headers. ability to read w/h

--- main.c
+++ main.c
@@ -8,47 +8,64 @@
 typedef unsigned char byte;
 typedef unsigned int byte_4;
 
-void print4ByteArray(byte_4* arr, int size)
+byte pngsig[4] = {137, 80, 78, 71};
+
+typedef struct PNG_HDR
 {
-    for (int i = 0; i < size; i++)
-    {
-        printf("->%04x\n", arr[i]);
-    }
-}
+    byte* fileData;
+    byte_4 width;
+    byte_4 height;
+} PNG_HDR ;
+
 
 void printByteArray(byte* byteArr, int size)
 {
     for (int i = 0; i < size; i++)
     {
-        printf("->%04x\n", byteArr[i]);
+        printf("->%02d\n", byteArr[i]);
     }
+}
+
+byte_4 combineByteIntoInt(byte* arr)
+{
+    return (arr[0] << 24 | arr[1] << 16 | arr[2] << 8 | arr[3]);
+}
+
+void initPNG_HDR(PNG_HDR* ptr)
+{
+    ptr->fileData = (byte *)calloc(12, sizeof(byte));
+}
+
+void readPNG_HDR(PNG_HDR* ptr)
+{
+    FILE* filePointer = fopen("img.png", "rb");
+    assert(filePointer != NULL);
+    fread(ptr->fileData, 1, 4, filePointer);
+    assert(memcmp(pngsig, ptr->fileData, sizeof(byte)) == 0);
+
+    fseek(filePointer, 16, SEEK_SET); // move to the end of the IHDR declaration
+    fread(&ptr->fileData[4], 1, 8, filePointer);
+
+    ptr->width = combineByteIntoInt(&ptr->fileData[4]);
+    ptr->height = combineByteIntoInt(&ptr->fileData[8]);
+
+    printf("Width/Height: %d/%d", ptr->width, ptr->height);
+
+    // printByteArray(ptr->fileData, 12);
+    fclose(filePointer);
+    free(filePointer);
 }
 
 int main()
 {
-    int byteArrSize = 8;    
-    byte pngsig[8] = {137, 80, 78, 71, 13, 10, 26, 10};
-    byte* fileData = (byte *)malloc(byteArrSize * sizeof(byte));
-    byte* wh = (byte *)malloc(13 * sizeof(byte));
 
-    FILE* filePointer;
-    filePointer = fopen("img.png", "rb");
-    assert(filePointer != NULL);
-    
-    fread(fileData, 1, byteArrSize, filePointer);
-    assert(memcmp(pngsig, fileData, sizeof(byte)) == 0);
-    fseek(filePointer, 16, SEEK_SET);
-    fread(wh, 1, 13, filePointer);
-    printByteArray(wh, 13);
-
-    printf("this is a png file");
+    PNG_HDR* png_header = (PNG_HDR *)malloc(sizeof(PNG_HDR));
+    initPNG_HDR(png_header);
+    readPNG_HDR(png_header);
 
     // REFER => https://gamedev.stackexchange.com/questions/102490/fastest-way-to-render-image-data-from-buffer
     // REFER => https://www.gamedev.net/forums/topic/683956-blit-a-byte-array-of-pixels-to-screen-in-sdl-fast/5320207/
     // REFER => https://progbook.org/png.html
-    // retro theme, menlo
-
-    // read each byte and gather little-endian 4 bytes from 4 different 1 bytes
 
     // get a RGBA array from this PNG file,
     // and then display it in the SDL blitting window
